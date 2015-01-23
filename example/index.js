fe45a0682fbb4635d94f9f99f1487f9f99bb730c
Vitor Leal <vitorleal1@gmail.com>
2015-01-23 15:16:10 -0200

    treat wearable data for each sensor

--- example/index.js
+++ example/index.js
@@ -3,25 +3,39 @@ var Wearable = require('../lib/wearable.js'),
       name: 'wV3_0E003976'
     });
 
+//Find for the kit
 kit.findWearable();
 
+//After kit connected
 kit.on('connected', function () {
   console.log("Connected to the kit");
   kit.ledON();
 
   setTimeout(function () {
-    kit.ledOFF();
-  }, 1000);
-
-  setTimeout(function () {
     kit.ledON('BLUE');
   }, 2000);
-
-  setTimeout(function () {
-    kit.ledON('GREEN');
-  }, 3000);
 });
 
-kit.on('data', function (data) {
-  console.log(data);
+//On disconnect
+kit.on('disconnected', function () {
+  console.log('Bye!');
+});
+
+//On data button 1
+kit.on('data:button1', function (data) {
+  if (data) {
+    kit.getTemperature();
+  }
+});
+
+//On data button 2
+kit.on('data:button2', function (data) {
+  if (data) {
+    kit.getLuminosity();
+  }
+});
+
+//On any data change
+kit.on('data', function () {
+  console.log(kit.data);
 });
