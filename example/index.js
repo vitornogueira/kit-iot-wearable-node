80eaa542fc33ac03ef6965da4d8587c110f48576
Vitor Leal <vitorleal1@gmail.com>
2015-02-05 19:30:54 -0200

    add x,y,z command to the wearable api

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 var Wearable = require('kit-iot-wearable'),
     kit = new Wearable({
-      name: 'wV3_0B003970'
+      name: 'wV3_0B0039AF'
     });
 
 //Find for the kit
@@ -11,10 +11,8 @@ kit.on('connected', function () {
   console.log("Connected to the kit");
 
   setInterval(function () {
-    kit.temperature();
-    kit.luminosity();
-    kit.playMelody();
-  }, 2000);
+    kit.accelerometer();
+  }, 200);
 });
 
 //On disconnect
@@ -22,26 +20,18 @@ kit.on('disconnected', function () {
   console.log('Bye!');
 });
 
-//On distance
-kit.on('data:distance', function (data) {
-  console.log('distance is %s', data);
+kit.on('data:accelerometer', function (data) {
+  console.log(data);
 });
 
-//On data button 1
-kit.on('data:button1', function (data) {
-  if (data) {
-    kit.playMelody();
-  }
+kit.on('data:accelerometer-x', function () {
+  console.log('X is %s', data);
 });
 
-//On data button 2
-kit.on('data:button2', function (data) {
-  if (data) {
-    kit.ledON('RED');
-  }
+kit.on('data:accelerometer-y', function () {
+  console.log('Y is %s', data);
 });
 
-//On any data change
-kit.on('data', function () {
-  console.log(kit.data);
+kit.on('data:accelerometer-z', function () {
+  console.log('Z is %s', data);
 });
