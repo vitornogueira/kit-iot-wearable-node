693224c89250404ee13149ad476b116a6b0ab3c4
Vitor Leal <vitorleal1@gmail.com>
2015-02-06 11:10:37 -0200

    update example

--- example/index.js
+++ example/index.js
@@ -20,15 +20,15 @@ kit.on('disconnected', function () {
   console.log('Bye!');
 });
 
-kit.on('data:accelerometer', function (data) {
-  console.log(data);
+kit.on('data:accelerometer', function () {
+  console.log(kit.data.accelerometer);
 });
 
-kit.on('data:accelerometer-x', function () {
+kit.on('data:accelerometer-x', function (data) {
   console.log('X is %s', data);
 });
 
-kit.on('data:accelerometer-y', function () {
+kit.on('data:accelerometer-y', function (data) {
   console.log('Y is %s', data);
 });
 
