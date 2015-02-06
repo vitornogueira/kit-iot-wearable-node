6c862ec7cc1184c80e1aeb2aea47e6454551dfd6
Vitor Leal <vitorleal1@gmail.com>
2015-02-06 11:20:33 -0200

    update the example

--- example/index.js
+++ example/index.js
@@ -17,21 +17,25 @@ kit.on('connected', function () {
 
 //On disconnect
 kit.on('disconnected', function () {
-  console.log('Bye!');
+  console.log('Disconnected from the wearable!');
 });
 
+//Return the 3 axis of the accelerometera ex: { x: 10, y: 5, z -10 }
 kit.on('data:accelerometer', function () {
   console.log(kit.data.accelerometer);
 });
 
+//Return only the x axis
 kit.on('data:accelerometer-x', function (data) {
   console.log('X is %s', data);
 });
 
+//Return only the y axis
 kit.on('data:accelerometer-y', function (data) {
   console.log('Y is %s', data);
 });
 
+//Return only the z axis
 kit.on('data:accelerometer-z', function () {
   console.log('Z is %s', data);
 });
